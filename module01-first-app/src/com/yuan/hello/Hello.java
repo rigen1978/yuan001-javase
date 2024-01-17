eec841a821361dae37370b9d99e596fbd90754e1
rigen1978 <1768363003@qq.com>
2024-01-17 10:06:56 +0900

    0001:javasebasic(初めてのjavaプログラミング)

---
+++ module01-first-app/src/com/yuan/hello/Hello.java
@@ -0,0 +1,16 @@
+package com.yuan.hello;
+
+/**
+ * @className: Hello
+ * @package: com.yuan.hello
+ * @description:
+ * @author: liyuan
+ * @create: 2024/01/17 1:26
+ * @version: 1.0
+ */
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World~~");
+    System.out.println("文字化けのテスト");
+  }
+}
